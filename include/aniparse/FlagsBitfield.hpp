500d7a4d79956f25b7b67123c3211c970cc7dc0e
Toilettrauma <macosinternal@gmail.com>
2026-07-03 03:42:00 +0300

    fix: Stable bitset ABI, like format/regex

--- include/aniparse/FlagsBitfield.hpp
+++ include/aniparse/FlagsBitfield.hpp
@@ -5,17 +5,9 @@
  */
 #pragma once
 #include "aniparse/detail/StrongBitset.hpp"
-#ifdef __cpp_lib_constexpr_bitset
-#include <bitset>
-#else
 #include "aniparse/detail/BitsetLite.hpp"
-#endif
 
 namespace aniparse {
 template <size_t BitCount, typename Tag>
-#ifdef __cpp_lib_constexpr_bitset
-using FlagsBitfield = detail::StrongBitset<std::bitset<BitCount>, Tag>;
-#else
 using FlagsBitfield = detail::StrongBitset<detail::BitsetLite<BitCount>, Tag>;
-#endif
 } // namespace aniparse
